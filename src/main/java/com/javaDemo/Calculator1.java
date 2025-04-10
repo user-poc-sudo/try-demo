9847219fd5dc3c5c577697d98b519052a8ea4ad6
deeksha <agarwal.deeksha1998@gmail.com>
2025-04-10 15:08:17 +0530

    Create Calculator1.java

---
+++ src/main/java/com/javaDemo/Calculator1.java
@@ -0,0 +1,30 @@
+package com.javaDemo;
+
+public class Calculator1 {
+
+
+        public Calculator1()
+        {
+            System.out.println("Calculator() object created....");
+        }
+
+        public int add(int a, int b)
+        {
+            return a+b;
+        }
+        public int sub(int a, int b)
+        {
+            return a-b;
+        }
+
+
+
+    public static void main(String[] args) {
+        Calculator1 cal= new Calculator1();
+        cal.add(3,6);
+        cal.sub(6,1);
+    }
+    }
+
+
+
