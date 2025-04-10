f62e3c4698cff3951943df7c1be41c2ad16fe6be
deeksha <agarwal.deeksha1998@gmail.com>
2025-04-10 15:14:00 +0530

    Update Calculator1.java

--- src/main/java/com/javaDemo/Calculator1.java
+++ src/main/java/com/javaDemo/Calculator1.java
@@ -16,13 +16,25 @@ public class Calculator1 {
         {
             return a-b;
         }
-
+        public int mul(int a, int b)
+        {
+            return a*b;
+        }
+        public int div(int a, int b)
+        {
+            if(b==0)
+                throw new ArithmeticException();
+               
+            return a/b;
+        }
 
 
     public static void main(String[] args) {
         Calculator1 cal= new Calculator1();
         cal.add(3,6);
         cal.sub(6,1);
+        cal.mul(2,5);
+        cal.div(10,5);
     }
     }
 
